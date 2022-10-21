2010691cde18132c68765b6601ea5dd5e3291ddd
Dion <dionsu@hotmail.de>
2022-10-21 20:05:53 +0200

    chapter 3 and modify gitignore for executable

---
+++ chap-03/chap03_01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    cout << "Please enter your name: \n";
+    string first_name;
+    cin >> first_name;
+    cout << "Hello, " << first_name << "!\n";
+}
