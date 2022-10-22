d530f89a3661010d97aca2978cf97dd23d1e115d
Dion <dionsu@hotmail.de>
2022-10-22 03:14:21 +0200

    chap 3 finish

--- chap-03/README.md
+++ chap-03/README.md
@@ -1,3 +1,91 @@
 # Objects, Types and Values
 
-![alt text](https://i.imgur.com/2Bms3pc.png)
+1. What is meant by the term prompt?
+
+It is a message shown up on the screen to guide user while input a data.
+2. Which operator do you use to read into a variable?
+
+>>
+3. If you want the user to input an integer value into your program for a
+variable named number , what are two lines of code you could write to
+ask the user to do it and to input the value into your program?
+
+int number;
+cout << "Input integer: ";
+cin >> number;
+
+4. What is \n called and what purpose does it serve?
+
+special character, create new line
+5. What terminates input into a string?
+
+white space
+6. What terminates input into an integer?
+
+
+7. How would you write
+
+cout << "Hello, ";
+cout << fi rst_name;
+cout << "!\n";
+
+as a single line of code?
+
+cout << "Hello, " << first_name << "\n";
+
+8. What is an object?
+
+some memory that holds a value of a given type
+9. What is a literal?
+
+a way of writting to state a value is a particular type
+10. What kinds of literals are there?
+
+"string"
+23 integer
+23.3 double
+'c'  char
+true bool
+11. What is a variable?
+
+named object
+12. What are typical sizes for a char , an int , and a double ?
+
+char 1 byte
+int 4 byte
+double 8 byte
+13. What measures do we use for the size of small entities in memory, such
+as int s and string s?
+14. What is the difference between = and == ?
+
+= is operator for assignment
+== is logical comparison equal
+15. What is a definition?
+
+a declaration that sets aside memory for an object.
+16. What is an initialization and how does it differ from an assignment?
+
+init = giving a variable its initial value
+assignment = giving a variable a new value
+17. What is string concatenation and how do you make it work in C++?
+
+addition of two strings variable resulting one string consists of those two
+18. Which of the following are legal names in C++? If a name is not legal,
+why not?
+
+This_little_pig: legal (usually used for user defined type)
+latest thing: not, name should not have space
+MiniMineMine: legal (but usually for class)
+This_1_is fine: not legal
+the_$12_method: not legal
+number : legal
+2_For_1_special: not legal
+_this_is_ok: better not, _ at begining reserved for internal variable
+correct?: not legal, spec char
+19. Give five examples of legal names that you shouldn’t use because they are
+likely to cause confusion.
+20. What are some good rules for choosing names?
+21. What is type safety and why is it important?
+22. Why can conversion from double to int be a bad thing?
+23. Define a rule to help decide if a conversion from one type to another is
+safe or unsafe.
--- chap-03/chap03_01.cpp
+++ chap-03/chap03_01.cpp
@@ -6,6 +6,144 @@
 
 using namespace std;
 
+int sec04_operations() {
+    cout << "Please enter floadting point: ";
+    double n;
+    cin >> n;
+    cout << "n==" << n
+         << "\nn+1 == " << n+1
+         << "\nthree times n == " << 3*n
+         << "\ntwice n == " << n+n
+         << "\nn squared == " << n*n
+         << "\nhalve of n == " << n/2
+         << "\nsquare root of n == " << sqrt(n)
+         << "\n";
+    return 0;
+}
+
+void sec04_operations_tt01(){
+    cout << "Please enter integer: ";
+    int n; // a/b * b + a%b == a is guaranteed for int
+    cin >> n;
+    double n_d = n;
+    cout << "n==" << n
+         << "\nn+1 == " << n+1
+         << "\nthree times n == " << 3*n
+         << "\ntwice n == " << n+n
+         << "\nn squared == " << n*n
+         << "\nhalve of n == " << n/2
+         << "\nsquare root of n == " << sqrt(n_d)
+         << "\n";
+}
+
+void sec04_string_comparison(){
+    cout << "Please enter two names\n";
+    string first;
+    string second;
+    cin >> first >> second;
+    if (first == second) cout << "That is the same name twice\n";
+    if (first < second) cout << first << " is alphabetically before " << second << "\n";
+    if (first > second) cout << first << " is alphabetically after " << second << "\n";
+}
+
+int sec05_ex(){
+    string previous = " ";
+    string current;
+    while (cin>>current){
+        if (previous == current){
+            cout << "repeated word: " << current << "\n";
+        }
+        previous = current;
+    }
+    return 0;
+}
+
+void sec06_ex(){
+    string previous = " ";
+    string current;
+    int number_of_words = 0;
+    while (cin>>current){
+        ++number_of_words;
+        if (previous == current){
+            cout << "word number " << number_of_words <<" repeated: " << current << "\n";
+        }
+        previous = current;
+    }
+}
+
+void sec_09_safe_conversion(){
+    char c = 'x';
+    int i1 = c;
+    int i2 = 'x';
+    char c2 = i1;
+    cout << c << ' ' << i1 << ' ' << c2 << endl;
+}
+
+void sec_09_unsafe_conversion(){
+    int a = 2000;
+    char c = a; // large int to small char
+    int b = c;
+    if (a!=b){
+        cout << "oops!: " << a << "!=" << b << endl;
+    } else {
+        cout << "Wow! We have large characters\n";
+    }
+}
+
+void sec_09_unsafe_conversion_2(){
+    double d = 0;
+    while (cin>>d) {
+        int i=d;
+        char c=i;
+        int i2 = c;
+
+        cout << "input double == " << d
+            << "\ndouble2int == " << i
+            << "\nchar2int == " << i2
+            << "\nint2char(" << c << ")\n";
+    }
+}
+
+void drill_01(){
+    cout << "Enter the name of the person you want to write:\n";
+    string name;
+    cin >> name;
+    cout << "Dear " << name << ",\n";
+    cout << "   How are you?" << "\n";
+    cout << "   There is something I want to tell you" << "\n";
+    cout << "Enter the name of a friend in your mind: " << "\n";
+    string freind_name;
+    cin >> freind_name;
+    cout << "   Have you seen " << freind_name << " lately?" << "\n";
+    cout << "Enter sex (f/m): ";
+    char friend_sex;
+    cin >> friend_sex;
+    if (friend_sex == 'm') {
+        cout << "   If you see " << freind_name << " please call me." << "\n";
+    } else if (friend_sex == 'f') {
+        cout << "   if you see " << freind_name << " please ask her to call me." << "\n";
+    } else {
+        cout << "Wrong sex input";
+    }
+    cout << "Enter the age: ";
+    int age;
+    cin >> age;
+    if (age < 0 or age > 110) {
+        cout << "You are kidding\n";
+    } else {
+        cout << "   I heard you just had a birthday and you are now " << age << " years old.\n";
+    }
+
+    if (age < 12) {
+        cout << "   Next year you will be " << age+1 << " years old.\n";
+    } else if (age == 17) {
+        cout << "   Next year you will be able to vote!" << "\n";
+    } else if (age > 70) {
+        cout << "   Enjoy your retirement!\n";
+    }
+    cout << "   Your sicerely\n\n\n";
+    cout << "   Dion\n";
+}
 
 
 int main() {
@@ -34,5 +172,41 @@ int main() {
     cout << "Hello, " << first_name << " (age " << age*12 << " Months)\n";
 
     // 3.4 Operations and Oparators
-    
+    sec04_operations();
+    sec04_operations_tt01();
+    sec04_string_comparison();
+
+    // 3.5. Assignment and Initialization
+    // sec05_ex();
+
+    // 3.6 Composite Assignment Operators
+    // sec06_ex();
+
+    // 3.7 Names
+    // 3.8 Types and Objects
+    // 3.9 Type Safety
+    /* 
+        Safe conversion
+        bool to char
+        bool to int
+        bool to double
+        char to int
+        char to double
+        int to double
+    */
+   sec_09_safe_conversion();
+   /*
+    unsafe conversion:
+    double to int
+    double to char
+    double to bool
+    int to char
+    int to bool
+    char to bool
+   */
+   // sec_09_unsafe_conversion();
+   // sec_09_unsafe_conversion_2();
+
+   // drill
+    drill_01();
 }
